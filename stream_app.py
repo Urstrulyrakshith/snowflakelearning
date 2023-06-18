f844ffa05ead5e08bd8b197217ffaf7f2222a811
Urstrulyrakshith <136587546+Urstrulyrakshith@users.noreply.github.com>
2023-06-19 02:28:46 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -37,4 +37,4 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
